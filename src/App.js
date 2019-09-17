dc30c9c8161124d556681b520f9b024ee2d88289
hejuhenryk <sawczuk.marcin@gmail.com>
2019-09-17 14:03:17 +0200

    leazyRouting

--- src/App.js
+++ src/App.js
@@ -4,29 +4,45 @@ import { connect } from 'react-redux'
 import { withRouter, Redirect, Switch, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import CheckOut from './containers/CheckOut/CheckOut'
-import Orders from './containers/Orders/Orders'
-import Logout from './containers/Authorisation/Logout/Logout'
-import assyncComponent from './hoc/asyncComponent/asyncComponent'
-import Authorisation from './containers/Authorisation/Authorisation'
+// import CheckOut from './containers/CheckOut/CheckOut'
+// import Orders from './containers/Orders/Orders'
+// import Logout from './containers/Authorisation/Logout/Logout'
+// import Authorisation from './containers/Authorisation/Authorisation'
+import asyncComponent from './hoc/asyncComponent/asyncComponent'
 import * as action from './store/actions/actionIndex'
+
+const asyncCheckOut = asyncComponent( () => {
+    return import('./containers/CheckOut/CheckOut')
+})
+
+const asyncOrders = asyncComponent( () => {
+    return import('./containers/Orders/Orders')
+})
+
+const asynkLogout = asyncComponent( () => {
+    return import('./containers/Authorisation/Logout/Logout')
+})
+
+const asyncAuthorisation = asyncComponent( () => {
+    return import('./containers/Authorisation/Authorisation')
+})
 
 const App = props => {
     useEffect(() => {
         props.onCheckIfAuth()
-    }, [props])
+    }, [props.isAuth])
 
     const publicRoutes = <Switch>
-        <Route path='/auth' component={Authorisation} />
+        <Route path='/auth' component={asyncAuthorisation} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
     </Switch>
 
     const protectedRoutes = <Switch>
-        <Route path='/checkout' component={CheckOut} />
-        <Route path='/auth' component={Authorisation} />
-        <Route path='/orders' component={Orders} />
-        <Route path='/logout' component={Logout} />
+        <Route path='/checkout' component={asyncCheckOut} />
+        <Route path='/auth' component={asyncAuthorisation} />
+        <Route path='/orders' component={asyncOrders} />
+        <Route path='/logout' component={asynkLogout} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
     </Switch>
